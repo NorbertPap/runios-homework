64f9dd3ecc3333af3236976983984e7ab878da41
Norbert <garrote713@gmail.com>
2021-05-13 18:48:05 +0200

    Refactor: removed unused packages, corrected wrong naming for router

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 require('dotenv').config();
 const knex = require('./db/db');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/photos');
+const photosRouter = require('./routes/photos');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/photos', usersRouter);
+app.use('/photos', photosRouter);
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000')
